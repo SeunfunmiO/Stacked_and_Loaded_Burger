9c75f47626251fb06b7f0c7cda46bf263738a4fc
seunfunmi <seunfunmisore89@gmail.com>
2025-12-03 12:04:04 +0100

    first commit on stacked and loaded burger website

---
+++ app/menu/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Page = () => {
+  return (
+    <div className='bg:white dark:black'>
+        <h1 className="font-bold text-2xl">Our Menu</h1>
+    </div>
+  )
+}
+
+export default Page
--- postcss.config.mjs
+++ postcss.config.mjs
@@ -2,6 +2,15 @@ const config = {
   plugins: {
     "@tailwindcss/postcss": {},
   },
+
+  darkMode:["class"],
+  content:[
+    "./app/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+  ],
+  theme:{
+    extend:{},
+  }
 };
 
 export default config;
