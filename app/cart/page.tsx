3c711c7447bc8372837b181dfdb364b225f711df
seunfunmi <seunfunmisore89@gmail.com>
2026-01-16 10:05:58 +0100

    redploying

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -8,10 +8,6 @@ const Page = () => {
     return (
         <div >
             <NavigationMenuDemo />
-
-            <h1 className="font-bold text-3xl flex py-10 gap-3 justify-center text-black bg-white">
-                <ShoppingCart size={30} /> CART
-            </h1>
             <div className='bg-white h-full flex /items-center flex-col'>
 
                 <CartItems />
